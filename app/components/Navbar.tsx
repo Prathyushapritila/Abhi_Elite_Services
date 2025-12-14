0e23f5efe44d92edb93d58195cd5948f9f0c02e2
Pritila Prathyusha <pritilaprathyusha@Pritilas-MacBook-Air.local>
2025-12-14 16:15:20 -0600

    Update header logo to use AbhiPng-12.png

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -53,7 +53,7 @@ export default function Navbar() {
             aria-label="Abhi Synergies Home"
           >
             <img
-              src="/abhi-logo.png"
+              src="/AbhiPng-12.png"
               alt="Abhi Synergies"
               className="h-16 md:h-20 w-auto object-contain flex-shrink-0"
             />
