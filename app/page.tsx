e7f12b2d5940074c4b45e45619b45ec6cf888f93
Pritila Prathyusha <pritilaprathyusha@Pritilas-MacBook-Air.local>
2025-12-14 18:28:14 -0600

    Revert "Add subtle ABHI logo watermark as background across entire website - 35% desktop, 55% mobile, 0.05 opacity, pointer-events none"
    This reverts commit 86dba61cf855e609aed99716b2b055d3ea11a120.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 
 export default function Home() {
   return (
-    <main className="watermark-container">
+    <main>
       <Navbar />
       <Hero />
       <Services />
